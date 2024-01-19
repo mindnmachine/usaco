9f4dd21460d19ad1fbe8850f8225b7a7c14876e7
Pravin Bhandarkar <pravin@Pravins-MacBook-Pro.local>
2024-01-19 13:03:38 -0800

    pushing new code

---
+++ kattis/compoundwords.cpp
@@ -0,0 +1,25 @@
+#include"bits/stdc++.h"
+
+using namespace std;
+
+int main(){
+
+    vector<string> input;
+    set<string> output;
+    string arr;
+    
+    while (cin >> arr ) {
+        input.push_back(arr);
+    }
+
+    for( int i = 0; i < input.size(); i ++){
+        for( int j =  i+1; j < input.size(); j ++){
+            output.insert(input[i] + input[j]);
+            output.insert(input[j] + input[i]);
+            }
+        }
+    for (auto element : output) {
+        cout << element<<endl;
+    }
+
+  }
